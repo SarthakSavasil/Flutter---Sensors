9f58e8063f83e0df5b06b028c55aaba1a371f941
Sarthak Gupta <sg191107@gmail.com>
2021-08-10 11:39:08 +0530

    Uploaded

---
+++ Flutter Sensors/lib/main.dart
@@ -0,0 +1,210 @@
+
+import 'dart:io';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:sensors/sensors.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+
+class _MyHomePageState extends State<MyHomePage> {
+  double xA, yA, zA, xG, yG, zG;
+  //double x, y, z;
+
+  var _sensors = <String>['accelerometer', 'gyroscope'];
+  String _selectedSensor; // _sensors[0];
+
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    // _selectedSensor = _sensors[0];
+    super.initState();
+    accelerometerEvents.listen((AccelerometerEvent eventA) {
+      setState(() {
+        xA = eventA.x;
+        yA = eventA.y;
+        zA = eventA.z;
+        //x = xA;
+        //y = yA;
+        //z = zA;
+      });
+    }); //get the sensor data and set then to the data types
+
+    gyroscopeEvents.listen((GyroscopeEvent eventG) {
+      setState(() {
+        xG = eventG.x;
+        yG = eventG.y;
+        zG = eventG.z;
+      });
+    });
+  }
+  String gettext(double ACC , double GYR)
+  {
+    var text='';  
+    if(_selectedSensor == null)
+    {
+        text = '0';
+    }
+    else
+    {
+      if(_selectedSensor == 'accelerometer')
+      {
+        if(ACC == null)
+        {
+          text = '0';
+        }
+        else
+        {
+          text = ACC.toStringAsFixed(2);
+        }
+      }
+      else
+      {
+        if(GYR == null)
+        {
+          text = '0';
+        }
+        else
+        {
+          text =GYR.toStringAsFixed(2);
+        }
+      }
+    }
+    
+  return text;
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Flutter Sensor Library"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Text(
+                  "Select the sensor from the dropdown list",
+                  style:
+                      TextStyle(fontSize: 12.0, fontWeight: FontWeight.normal),
+                ),
+              ),
+              DropdownButton<String>(
+                value: _selectedSensor,
+                onChanged: (val) {
+                  setState(() {
+                    // updating the state
+                    _selectedSensor = val;
+
+                    /*if (_selectedSensor == 'accelerometer') {
+                      // do something here
+                      x = xA;
+                      y = yA;
+                      z = zA;
+                    }
+                    if (_selectedSensor == 'gyroscope') {
+                      // do something here
+                      x = xG;
+                      y = yG;
+                      z = zG;
+                    }*/
+                  });
+                },
+                items: _sensors.map((String elem) {
+                  return DropdownMenuItem<String>(
+                    value: elem,
+                    child: Text(elem),
+                  );
+                }).toList(),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+              ),
+              Table(
+                border: TableBorder.all(
+                    width: 2.0,
+                    color: Colors.blueAccent,
+                    style: BorderStyle.solid),
+                children: [
+                  TableRow(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          "X axis: ",
+                          style: TextStyle(fontSize: 20.0),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          gettext(xA,xG),
+                          style: TextStyle(fontSize: 20.0),
+                        ),
+                      )
+                    ],
+                  ),
+                  TableRow(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          "Y axis: ",
+                          style: TextStyle(fontSize: 20.0),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                            gettext(yA, yG),
+                            style: TextStyle(fontSize: 20.0)),
+                      )
+                    ],
+                  ),
+                  TableRow(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          "Z axis: ",
+                          style: TextStyle(fontSize: 20.0),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                            gettext(zA, zG),
+                            style: TextStyle(fontSize: 20.0)),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ));
+  }
+}
